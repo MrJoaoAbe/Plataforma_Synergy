7fd8545ef01a6e0dc995575ad97c5bf46498b78b
MrJoaoAbe <joaovictorsouzaabe@gmail.com>
2025-11-12 19:33:00 -0300

    add logica sign nav

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,11 +1,17 @@
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faHouse, faUserDoctor, faUserGroup, faComment } from "@fortawesome/free-solid-svg-icons";
 import { NavLink } from "react-router-dom";
+import profileSemFoto from '../assets/profileSemFoto.png'
 
 function Navbar() {
+    const usuarioLogado = JSON.parse(localStorage.getItem("UsuarioLogado"));
+
+    // Definindo valores padrão caso o localStorage seja null
+    const nomeLogado = usuarioLogado?.nome || "ENTRAR";
+    const fotoLogado = usuarioLogado?.foto || profileSemFoto;
+
     return (
         <div className="bg-[#859F74] h-20 grid grid-cols-8 text-xl gap-20 text-white">
-
             <div className="flex items-center col-start-1">
                 <h1 className="font-extralight text-3xl ml-10">SYNERGY</h1>
             </div>
@@ -13,55 +19,45 @@ function Navbar() {
             <div className="flex items-center col-start-4">
                 <NavLink
                     to="/"
-                    className={({ isActive }) => `flex items-center justify-center text-4xl h-20 ${isActive ? "border-b-4 border-white" : ""} text-white`}>
+                    className={({ isActive }) => `flex items-center justify-center text-4xl h-20 ${isActive ? "border-b-4 border-white" : ""}`}>
                     <FontAwesomeIcon icon={faHouse} />
                 </NavLink>
             </div>
 
-
             <div className="flex items-center col-start-5">
                 <NavLink
                     to="/funcionarios"
-                    className={({ isActive }) => `flex items-center justify-center text-4xl h-20 ${isActive ? "border-b-4 border-white" : ""} text-white`}>
-                    <FontAwesomeIcon icon={faUserDoctor} className="text-white text-4xl" />
+                    className={({ isActive }) => `flex items-center justify-center text-4xl h-20 ${isActive ? "border-b-4 border-white" : ""}`}>
+                    <FontAwesomeIcon icon={faUserDoctor} />
                 </NavLink>
-
             </div>
 
             <div className="flex items-center col-start-6">
                 <NavLink
                     to="/seguidores"
-                    className={({ isActive }) => `flex items-center justify-center text-4xl h-20 ${isActive ? "border-b-4 border-white" : ""} text-white`}>
-                    <FontAwesomeIcon icon={faUserGroup} className="text-white text-4xl" />
+                    className={({ isActive }) => `flex items-center justify-center text-4xl h-20 ${isActive ? "border-b-4 border-white" : ""}`}>
+                    <FontAwesomeIcon icon={faUserGroup} />
                 </NavLink>
-
             </div>
 
             <div className="flex items-center col-start-7">
                 <NavLink
                     to="/mensagens"
-                    className={({ isActive }) => `flex items-center justify-center text-4xl h-20 ${isActive ? "border-b-4 border-white" : ""} text-white`}>
-                    < FontAwesomeIcon icon={faComment} className="text-white text-4xl" />
+                    className={({ isActive }) => `flex items-center justify-center text-4xl h-20 ${isActive ? "border-b-4 border-white" : ""}`}>
+                    <FontAwesomeIcon icon={faComment} />
                 </NavLink>
-
             </div>
 
             <div className="flex items-center justify-end col-start-8 mr-20">
                 <NavLink
-                    to="/login"
-                    className={({ isActive }) => `flex items-center justify-center h-20 ${isActive ? "border-b-4 border-white" : ""} text-white`}>
-                    <span>ENTRAR</span>
-                    <img src="" alt="" className="w-10 h-10 rounded-full" />
+                    to={usuarioLogado ? "/meuPerfil" : "/login"}
+                    className={({ isActive }) => `flex items-center justify-center h-20 ${isActive ? "border-b-4 border-white" : ""}`}>
+                    <span>{nomeLogado}</span>
+                    <img src={fotoLogado} className="w-10 h-10 rounded-full ml-5" />
                 </NavLink>
-
-
             </div>
         </div>
-    )
+    );
 }
 
-
-export default Navbar
-
-
-
+export default Navbar;
--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,15 +1,47 @@
 import { Link } from "react-router-dom"
+import { useState } from "react"
+import { useNavigate } from "react-router-dom";
 
 function Login() {
+
+    const navigate = useNavigate()
+    const [email, setEmail] = useState("");
+    const [senha, setSenha] = useState("");
+
+    const usuariosExistentes = JSON.parse(localStorage.getItem("Usuarios")) || [];
+
+    function handleSubmit(e) {
+        e.preventDefault();
+
+        if (senha === '' || email === '') {
+            alert("Preencha todos os campos");
+        } else {
+            const usuarioEncontrado = usuariosExistentes.find(
+                (u) => u.email === email && u.senha === senha
+            );
+
+            if (!usuarioEncontrado) {
+                alert("Senha ou Email estão errados");
+            } else {
+                alert(`Bem vindo ${usuarioEncontrado.nome}`);
+
+                localStorage.setItem("UsuarioLogado", JSON.stringify(usuarioEncontrado));
+                navigate("/")
+            }
+        }
+    }
+
     return (
         <div className="bg-[#DFDFDF] min-h-screen flex items-center justify-center">
             <div className="w-200 h-200 bg-white rounded-4xl shadow-2xl flex flex-col">
                 <h1 className="font-extralight text-4xl text-[#859F74] p-20 flex items-center ml-10 mt-15">LOGIN</h1>
                 <div>
-                    <form className="flex flex-col items-center gap-5">
-                        <input type="email" className="w-130 border-gray-200 border-4 h-13 rounded-3xl shadow-2xl pl-5 placeholder:text-[#859F74] placeholder:text-lg mb-10" placeholder="Email" />
-                        <input type="text" className="w-130 border-gray-200 border-4 h-13 rounded-3xl shadow-2xl pl-5 placeholder:text-[#859F74] placeholder:text-lg" placeholder="Senha" />
-                        <button className="bg-[#859F74] w-80 h-15 rounded-2xl text-white mt-15 mb-0">ENTRAR</button>
+                    <form onSubmit={handleSubmit} className="flex flex-col items-center gap-5">
+                        <input type="email" className="w-130 border-gray-200 border-4 h-13 rounded-3xl shadow-2xl pl-5 placeholder:text-[#859F74] placeholder:text-lg mb-10" placeholder="Email"
+                            onChange={(e) => setEmail(e.target.value)} />
+                        <input type="password" className="w-130 border-gray-200 border-4 h-13 rounded-3xl shadow-2xl pl-5 placeholder:text-[#859F74] placeholder:text-lg" placeholder="Senha"
+                            onChange={(e) => setSenha(e.target.value)} />
+                        <button type="submit" className="bg-[#859F74] w-80 h-15 rounded-2xl text-white mt-15 mb-0">ENTRAR</button>
                         <p className="pt-0 text-[#859F74]">Não possui uma conta, clique <Link to="/cadastro" className="font-black">AQUI</Link> para se criar uma</p>
                     </form>
                 </div>
