0ad19f9ed014bc3887dfadf3126d29598b7342cb
MrJoaoAbe <joaovictorsouzaabe@gmail.com>
2025-11-13 08:29:19 -0300

    add Criar Post

--- src/components/CardPostagens.jsx
+++ src/components/CardPostagens.jsx
@@ -1,23 +1,23 @@
 import FotoExemplo from "../assets/Example.jpg"
 
-function CardPostagens() {
+function CardPostagens({ key, id, autor, foto, titulo, mensagem, imagem }) {
     return (
         <div>
-            <div className="flex flex-col items-start justify-start bg-white rounded-4xl p-5 m-20 mt-2 shadow-2xl">
+            <div className="flex flex-col items-start justify-start bg-white rounded-4xl p-5 mt-5 mx-10  shadow-2xl">
                 <div className="flex flex-row gap-10">
                     <div className="flex flex-row items-center gap-5 justify-start">
-                        <img src={FotoExemplo} alt="Usuario1" className="w-10 rounded-full" />
-                        <p className="text-2xl text-[#859F74]">JOÃO ABE</p>
+                        <img src={foto ? foto : FotoExemplo} alt="Usuario1" className="w-10 rounded-full" />
+                        <p className="text-2xl text-[#859F74]">{autor}</p>
                     </div>
                 </div>
 
                 <div className="flex justify-center w-full">
-                    <img src={FotoExemplo} alt="Postagem" className="my-3 w-200 rounded-2xl" />
+                    {imagem && <img src={imagem} alt="Postagem" className="my-3 w-200 rounded-2xl" />}
                 </div>
 
                 <div className="flex flex-col items-start gap-2 text-[#859F74] text-md font-semibold">
-                    <p>TITULO DA POSTAGEM</p>
-                    <p>CONTEÚDO</p>
+                    <p>{titulo}</p>
+                    <p>{mensagem}</p>
                 </div>
 
             </div>
--- src/pages/CriarPost.jsx
+++ src/pages/CriarPost.jsx
@@ -1,8 +1,68 @@
 import { Link } from "react-router-dom"
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faRightFromBracket } from "@fortawesome/free-solid-svg-icons";
+import { useEffect, useState } from "react";
+import { useNavigate } from "react-router-dom";
 
 function Home() {
+
+    const navigate = useNavigate()
+
+
+    const API = import.meta.env.VITE_FUNCIONARIOS_API
+    const usuarios = 'usuarios'
+
+    const [criarPost, setCriarPost] = useState([])
+
+    const usuarioLogado = JSON.parse(localStorage.getItem("UsuarioLogado"));
+
+
+    const [titulo, setTitulo] = useState('')
+    const [mensagem, setMensagem] = useState('')
+    const [imagem, setImagem] = useState('')
+
+
+
+    function handleSubmit(e) {
+        e.preventDefault()
+
+        const novoPost = {
+            autor: usuarioLogado.nome,
+            foto_autor: usuarioLogado.foto,
+            titulo: titulo,
+            mensagem: mensagem,
+            imagem: imagem,
+        }
+
+        fetch(`${API}${usuarios}/${usuarioLogado.id}`)
+            .then(res => res.json())
+            .then(usuario => {
+
+                const usuarioAtualizado = {
+                    ...usuario,
+                    postagens: [...(usuario.postagens || []), novoPost]
+                };
+
+
+                return fetch(`${API}${usuarios}/${usuarioLogado.id}`, {
+                    method: "PUT",
+                    headers: {
+                        "Content-Type": "application/json",
+                    },
+                    body: JSON.stringify(usuarioAtualizado),
+                });
+            })
+            .then(res => {
+                if (!res.ok) {
+                    throw new Error("Erro ao atualizar o usuário");
+                }
+                alert("Post criado com sucesso!");
+            })
+            .catch(err => console.error("Erro ao criar post:", err));
+
+        navigate('/')
+    }
+
     return (
         <div className="bg-[#EDEBEB] pb-10 rounded-4xl shadow-2xl flex flex-col">
             <div className="flex flex-row items-center justify-between px-10">
@@ -14,12 +74,15 @@ function Home() {
                 </Link>
             </div>
 
-            <div className="flex flex-col gap-10 justify-center items-center">
-                <input type="text" className="w-130 border-gray-200 border-4 h-13 rounded-3xl shadow-2xl pl-5 placeholder:text-[#859F74] placeholder:text-lg" placeholder="Titulo da postagem" />
-                <textarea type="text" className="w-130 h-80 border-gray-200 border-4 rounded-3xl shadow-2xl pl-5 placeholder:text-[#859F74] placeholder:text-lg" placeholder="Mensagem" />
-                <input type="text" className="w-130 border-gray-200 border-4 h-13 rounded-3xl shadow-2xl pl-5 placeholder:text-[#859F74] placeholder:text-lg" placeholder="URL da imagem" />
-                <button className="bg-[#859F74] w-80 h-15 rounded-2xl text-white mt-15 mb-0">CRIAR POSTAGEM</button>
-            </div>
+            <form onSubmit={handleSubmit} className="flex flex-col gap-10 justify-center items-center">
+                <input type="text" className="w-130 border-gray-200 border-4 h-13 rounded-3xl shadow-2xl pl-5 placeholder:text-[#859F74] placeholder:text-lg" placeholder="Titulo da postagem" required
+                    onChange={e => setTitulo(e.target.value)} />
+                <textarea type="text" className="w-130 h-80 border-gray-200 border-4 rounded-3xl shadow-2xl pl-5 placeholder:text-[#859F74] placeholder:text-lg" placeholder="Mensagem" required
+                    onChange={e => setMensagem(e.target.value)} />
+                <input type="text" className="w-130 border-gray-200 border-4 h-13 rounded-3xl shadow-2xl pl-5 placeholder:text-[#859F74] placeholder:text-lg" placeholder="URL da imagem"
+                    onChange={e => setImagem(e.target.value)} />
+                <button type="submit" className="bg-[#859F74] w-80 h-15 rounded-2xl text-white mt-15 mb-0">CRIAR POSTAGEM</button>
+            </form>
 
 
         </div>
