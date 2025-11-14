0877b2bfc38d9596b526339f61b46711a813a530
MrJoaoAbe <joaovictorsouzaabe@gmail.com>
2025-11-14 12:21:22 -0300

    fix darkMode

--- src/components/CardMensagem.jsx
+++ src/components/CardMensagem.jsx
@@ -3,25 +3,55 @@ import { Link } from "react-router-dom"
 import profileSemFoto from '../assets/profileSemFoto.png'
 
 function CardMensagem({ id, conteudo, nome, foto, horasAtras }) {
+
+    const usuarioLogado = JSON.parse(localStorage.getItem("UsuarioLogado")) || {
+        darkMode: false
+    };
+
     return (
         <div>
-            <Link to={`/mensagemDireta/${id}`}>
-                <div className="flex flex-col items-center justify-center bg-white rounded-4xl p-5 m-20 mt-3 shadow-2xl">
-                    <div className="flex flex-row gap-10">
-                        <img src={foto} className="w-20 rounded-full" />
-
-                        <div className="flex flex-col justify-center text-[#859F74] mr-20">
-                            <p className="text-4xl">{nome}</p>
+            {usuarioLogado.darkMode === false ?
+                //modo claro
+                <div>
+                    <Link to={`/mensagemDireta/${id}`}>
+                        <div className="flex flex-col items-center justify-center bg-white rounded-4xl p-5 m-20 mt-3 shadow-2xl">
                             <div className="flex flex-row gap-10">
-                                <p className="text-xl ml-5">{conteudo}</p>
-                                <p className="text-xl">{horasAtras}h</p>
+                                <img src={foto} className="w-20 rounded-full" />
+
+                                <div className="flex flex-col justify-center text-[#859F74] mr-20">
+                                    <p className="text-4xl">{nome}</p>
+                                    <div className="flex flex-row gap-10">
+                                        <p className="text-xl ml-5">{conteudo}</p>
+                                        <p className="text-xl">{horasAtras}h</p>
+                                    </div>
+
+
+                                </div>
                             </div>
+                        </div></Link>
+
+                </div>
+                :
+                //modo escuro
+                <div>
+                    <Link to={`/mensagemDireta/${id}`}>
+                        <div className="flex flex-col items-center justify-center bg-[#111411] rounded-4xl p-5 m-20 mt-3 shadow-2xl">
+                            <div className="flex flex-row gap-10">
+                                <img src={foto} className="w-20 rounded-full" />
+
+                                <div className="flex flex-col justify-center text-[#ffffff] mr-20">
+                                    <p className="text-4xl">{nome}</p>
+                                    <div className="flex flex-row gap-10">
+                                        <p className="text-xl ml-5">{conteudo}</p>
+                                        <p className="text-xl">{horasAtras}h</p>
+                                    </div>
 
 
-                        </div>
-                    </div>
-                </div></Link>
+                                </div>
+                            </div>
+                        </div></Link>
 
+                </div>}
 
         </div>
     )
